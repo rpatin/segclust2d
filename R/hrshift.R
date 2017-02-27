6c98b453880fc9cc3b7af0358d7cb20b4f87861a
rpatin <remi.patin@free.fr>
2017-02-27 14:33:51 +0000

    added simulation data

---
+++ R/hrshift.R
@@ -0,0 +1,17 @@
+#' Simulations of home-range shift
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{indice}{index of position}
+#'   \item{x}{x coordinates}
+#'   \item{y}{y coordinates}
+#'   \item{expectTime}{arbitrary POSIXct time}#'
+#' }
+
+"hrshift"
+
+# hrshift = readRDS("../HRshift.rds")
+# devtools::use_data(hrshift)
