96785206f6e46f3c22fa4f824e74f39d13f8ffce
Remi PATIN <patin@newcefe.newage.fr>
2018-08-13 08:36:29 +0000

    a bit of cleaning

--- R/segTraj_Gmean_simultanee.R
+++ R/segTraj_Gmean_simultanee.R
@@ -11,13 +11,15 @@ Gmean_simultanee<-function(Don,lmin,sameVar=FALSE)
   if(sameVar){
     ## every element of the list is the cost motrix for one signal
     matD_list=lapply(1:2,function(nb) {
-      Res=matrix(Inf,n,n)
-      z=Don[nb,] #depend de la forme des donnees
-      z2=z^2
-      z2i=cumsum(z2)
-      zi=cumsum(z)
-      z2i=z2i[lmin:n]
-      zi=zi[lmin:n]
+      Res = matrix(Inf,n,n)
+      z   = Don[nb,] #depend de la forme des donnees
+      z2  = z^2
+      z2i = cumsum_cpp(z2)
+      zi  = cumsum_cpp(z)
+      # z2i = cumsum(z2)
+      # zi  = cumsum(z)
+      z2i = z2i[lmin:n]
+      zi  = zi[lmin:n]
       Res[1,lmin:n]=z2i-((zi^2)/(lmin:n))
       nl=n-lmin+1
       for (i in 2:nl)
@@ -38,13 +40,15 @@ Gmean_simultanee<-function(Don,lmin,sameVar=FALSE)
     
     matD_list=lapply(1:2,function(nb) {
       Res=matrix(Inf,n,n)
-      z=Don[nb,] #depend de la forme des donnees
-      z2=z^2
-      z2i=cumsum(z2)
-      zi=cumsum(z)
-      z2i=z2i[lmin:n]
-      zi=zi[lmin:n]
-      
+      z   = Don[nb,] #depend de la forme des donnees
+      z2  = z^2
+      # z2i = cumsum(z2)
+      # zi  = cumsum(z)
+      z2i = cumsum_cpp(z2)
+      zi  = cumsum_cpp(z)
+      z2i = z2i[lmin:n]
+      zi  = zi[lmin:n]
+      # if(nb == 2) browser()
       Res[1,lmin:n]=(lmin:n)*log(( z2i-(zi^2)/(lmin:n) ) / (lmin:n) )
       nl=n-lmin+1
       for (i in 2:nl){
@@ -55,7 +59,7 @@ Gmean_simultanee<-function(Don,lmin,sameVar=FALSE)
         tmp_gmean <- (z2i-(zi^2)/(lmin:(n-i+1)))/(lmin:(n-i+1))
         if(any(tmp_gmean <= 0)){
           # browser()
-          stop(paste0("Problem with a segment starting at point ",i," (once subsampled) and ending between point ", i+lmin-1," and ",n,". Check for segment with potentially very low variance compared to the rest of the serie. This could happen if there are interpolation of trajectories or if data have not been properly cleaned (e.g. stationary gps points). The algorithm cannot work with series of (near)-identical points."))
+          stop(paste0("Problem with a segment starting at point ",i," (once subsampled) and ending at point ", i+lmin-1,". (or maybe after also). Please check if the segment has a potentially very low variance compared to the rest of the serie. This could happen if there are interpolation of trajectories or if data have not been properly cleaned (e.g. motionless gps points due to a fallen devices). The algorithm cannot work with series of (near-)identical points."))
         }
         Res[i,(i+lmin-1):n]=(lmin:(n-i+1))*(log(tmp_gmean))
         
